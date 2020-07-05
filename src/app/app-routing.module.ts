903738cecc26862d85f3274d9b4190f18a32ace3
HUB-DOM <huraccahuab@gmail.com>
2020-07-04 21:12:59 -0500

    ng v10

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+import { DetailsProductComponent } from './products/details-product/details-product.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'products', component: ProductListComponent},
+  {path:'product/:id', component:DetailsProductComponent},
+  {path:'**', component:NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/shared/data.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  private products = [
+    {
+      id:1,
+      name: 'egg',
+      category: 'Food',
+      description: 'Lorem egg',
+      price: 1,
+    },
+    {
+      id:2,
+      name: 'Coca Cola',
+      category: 'Drink',
+      description: 'Lorem drink',
+      price: 2,
+    },
+    {
+      id:3,
+      name: 'Cookies',
+      category: 'Food',
+      description: 'Lorem Cookies',
+      price: 4,
+    },
+  ];
+
+  getAllProducts(){
+    return this.products;
+  }
+
+  getProductsById(id){
+    return this.products.filter(product => product.id == id);
+  }
+
+
+}
